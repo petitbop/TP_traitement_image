b9a1b66cfa75cf4fe20d8e9f287c373f47d1f5ce
typhomnt <typhomnt@voila.fr>
2015-03-09 12:34:19 +0100

    filtre gaussien + makefile.

--- TP2/scr/gaussian.c
+++ TP2/scr/gaussian.c
@@ -1,17 +1,27 @@
 #include "gaussian.h"
 #include "pgm.h"
+#include "math.h"
 
 
 
-void  gaussian_filter(double ** entree){
+double fft_gaussian(double sigm, int i, int j, int nl, int nc){
+    return exp(-2*pow(M_PI,2)*pow(sigm,2)*(pow((i-((double)nl/(double)2))/(double)nl,2) + pow((j-((double)nc/(double)2))/(double)nc,2)));
+}
 
+void  gaussian_filter(double ** entree_re,double **entree_im ,int nl,int nc){
+    for(int i = 0 ; i < nl ; i++){
+	for(int j = 0 ; j < nc ; j++){
+	    entree_re[i][j] *= fft_gaussian(2,i,j,nl,nc);
+	    entree_im[i][j] *= fft_gaussian(2,i,j,nl,nc);
+	}
+    }
 }
 
 
-main (int ac, char **av) {  /* av[1] contient le nom de l'image, av[2] le nom du resultat . */
+int main (int ac, char **av) {  /* av[1] contient le nom de l'image, av[2] le nom du resultat . */
   int nb,nl,nc, oldnl,oldnc;
   unsigned char ** im1=NULL;
-  double ** im3 ,** im4,** im5, ** im6, ** im7, **im8, **im9,**im10, **im11;
+  double ** im3 ,** im4,** im5, ** im6, ** im7, **im8, **im9,**im10, **im11, **im12, **im13;
   if (ac < 3) {printf("Usage : %s entree sortie \n",av[0]); exit(1); }
   /* Lecture d'une image pgm dont le nom est passe sur la ligne de commande */
   im1=lectureimagepgm(av[1],&nl,&nc);
@@ -27,13 +37,19 @@ main (int ac, char **av) {  /* av[1] contient le nom de l'image, av[2] le nom du
   
   fft(im4, im5, im6, im7, nl, nc);
   
+  im8 = alloue_image_double(nl,nc); im9 = alloue_image_double(nl,nc);
+  
   fftshift(im6, im7, im8, im9, nl, nc);
 
   im10 =  alloue_image_double(nl,nc); im11 = alloue_image_double(nl,nc);
   
-  gaussian_filter(im8);
+  gaussian_filter(im8,im9,nl,nc);
 
-  ifft(im8, im9, im10, im11, nl, nc);
+  im12 = alloue_image_double(nl,nc) ;  im13 = alloue_image_double(nl,nc) ;
+  
+  fftshift(im8,im9,im12,im13, nl, nc);
+
+  ifft(im12, im13, im10, im11, nl, nc);
   	/* Conversion en entier8bits de la partie reelle de la fftinverse, 
 	   Suppresion des 0 qui ont servi a completer en utilisant la fonction crop
 	   Sauvegarde au format pgm de cette image qui doit etre identique a 'linverse video 
--- TP2/scr/gaussian.h
+++ TP2/scr/gaussian.h
@@ -1,8 +1,8 @@
 #ifndef _GAUSSIAN
 #define _GAUSSIAN
 
-double fft_gaussian(double sigm, int i, int j);
+double fft_gaussian(double sigm, int i, int j, int nl, int nc);
 
-double** gaussian_filter(double ** entree);
+void gaussian_filter(double ** entree,double **entree_im,int nl,int nc);
 
 #endif
